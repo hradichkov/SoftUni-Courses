8ba7fdeec6ca7e3d146db2755140a940d2c0b50d
Hristiqn Radichkov <hristiqnradichkov@gmail.com>
2022-08-24 20:17:12 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Java/P03Arrays/P05EvenAndOddSubtraction.java
@@ -0,0 +1,24 @@
+package P03Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P05EvenAndOddSubtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int oddSum = 0;
+        int evenSum = 0;
+
+        for (int number : numArr) {
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+        }
+        System.out.println(evenSum - oddSum);
+    }
+}
