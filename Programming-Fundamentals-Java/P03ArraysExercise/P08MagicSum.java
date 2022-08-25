1491fa29f1a76eda010e8457a25f24c60d9f0d78
Hristiqn Radichkov <hristiqnradichkov@gmail.com>
2022-08-25 14:23:06 +0300

    Add files via upload
    Program, which prints all unique pairs in an array of integers whose sum is equal to a given number.

---
+++ Programming-Fundamentals-Java/P03ArraysExercise/P08MagicSum.java
@@ -0,0 +1,21 @@
+package P03ArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P08MagicSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == num) {
+                    System.out.printf("%d %d%n", array[i], array[j]);
+                }
+            }
+        }
+    }
+}
