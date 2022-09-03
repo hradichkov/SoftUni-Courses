cc5811919db4da5e6183248a554c244c99a352f3
Hristiqn Radichkov <hristiqnradichkov@gmail.com>
2022-09-03 10:52:06 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Java/P06ObjectsAndClassesExercise/P03OpinionPoll.java
@@ -0,0 +1,48 @@
+package P06ObjectsAndClassesExercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P03OpinionPoll {
+
+    static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int getAge() {
+            return this.age;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Person> personList = new ArrayList<>() {};
+
+        for (int i = 0; i < n; i++) {
+            String[] data = scanner.nextLine().split(" ");
+            String name = data[0];
+            int age = Integer.parseInt(data[1]);
+
+            Person person = new Person(name, age);
+            personList.add(person);
+        }
+
+        for (Person person : personList) {
+            if (person.getAge() > 30) {
+                System.out.printf("%s - %d%n", person.getName(), person.getAge());
+            }
+        }
+    }
+}
