249fb9a328c1fae6c375375dec1d541639d09afa
Hristiqn Radichkov <hristiqnradichkov@gmail.com>
2022-09-03 10:51:35 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Java/P06ObjectsAndClassesExercise/P01AdvertisementMessage.java
@@ -0,0 +1,27 @@
+package P06ObjectsAndClassesExercise;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class P01AdvertisementMessage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Random randomGenerator = new Random();
+
+        String[] phrases = {"Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
+        String[] events = {"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"};
+        String[] authors = {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+        String[] cities = {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+
+        for (int i = 0; i < n; i++) {
+            int randomPhrase = randomGenerator.nextInt(phrases.length);
+            int randomEvent = randomGenerator.nextInt(events.length);
+            int randomAuthor = randomGenerator.nextInt(authors.length);
+            int randomCity = randomGenerator.nextInt(cities.length);
+
+            System.out.printf("%s %s %s - %s%n", phrases[randomPhrase], events[randomEvent], authors[randomAuthor], cities[randomCity]);
+        }
+    }
+}
