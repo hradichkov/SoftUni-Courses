83f9947a28253c4d84e99776582746a3bcb5e0f6
Hristiqn Radichkov <hristiqnradichkov@gmail.com>
2022-09-07 14:34:58 +0300

    MultiplyBigNumber problem solved

---
+++ Programming-Fundamentals-Java/P08TextProcessingExercise/P05MultiplyBigNumber.java
@@ -0,0 +1,15 @@
+package P08TextProcessingExercise;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P05MultiplyBigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger num1 = new BigInteger(scanner.nextLine());
+        BigInteger num2 = new BigInteger(scanner.nextLine());
+
+        System.out.println(num1.multiply(num2));
+    }
+}
