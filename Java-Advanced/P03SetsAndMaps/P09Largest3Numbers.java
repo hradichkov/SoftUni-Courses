6046f7302a888507ca96a9b4d38d2a1d37f08412
Hristiqn Radichkov <hristiqnradichkov@gmail.com>
2022-09-15 16:10:24 +0300

    Add files via upload

---
+++ Java-Advanced/P03SetsAndMaps/P09Largest3Numbers.java
@@ -0,0 +1,17 @@
+package P03SetsAndMaps;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class P09Largest3Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .map(String::valueOf)
+                .collect(Collectors.joining(" ")));
+    }
+}
