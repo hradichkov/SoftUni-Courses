d7f113c9a99bfc086776644db6d100ce0981e9a0
hradichkov <hristiqnradichkov@gmail.com>
2022-09-28 18:08:52 +0300

    JarOfT problem solved

---
+++ Java-Advanced/P07Generics/P01JarOfT/Main.java
@@ -0,0 +1,10 @@
+package P07Generics.P01JarOfT;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
---
+++ Java-Advanced/P07Generics/P01JarOfT/P01Jar.java
@@ -0,0 +1,19 @@
+package P07Generics.P01JarOfT;
+
+import java.util.ArrayDeque;
+
+public class P01Jar<E> {
+    private ArrayDeque<E> elements;
+
+    public P01Jar() {
+        this.elements = new ArrayDeque<>();
+    }
+
+    public void add(E element) {
+        elements.push(element);
+    }
+
+    public E remove() {
+        return elements.pop();
+    }
+}
