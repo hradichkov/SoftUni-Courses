f23be496fdca3adaf9ccaf35006d0771e9a29e13
hradichkov <hristiqnradichkov@gmail.com>
2022-10-11 18:17:21 +0300

    TrafficLights problem solved

---
+++ OOP/OOP/P01WorkingWithAbstractionExercises/P04TrafficLights/Color.java
@@ -0,0 +1,11 @@
+package P01WorkingWithAbstractionExercises.P04TrafficLights;
+
+public enum Color {
+    RED,
+    GREEN,
+    YELLOW;
+
+//    public Color next(){
+//        return values()[(this.ordinal() + 1) % values().length];
+//    }
+}
---
+++ OOP/OOP/P01WorkingWithAbstractionExercises/P04TrafficLights/TrafficLight.java
@@ -0,0 +1,27 @@
+package P01WorkingWithAbstractionExercises.P04TrafficLights;
+
+public class TrafficLight {
+    private Color currentColor;
+
+    public TrafficLight(Color currentColor) {
+        this.currentColor = currentColor;
+    }
+
+    public Color getCurrentColor() {
+        return currentColor;
+    }
+
+    public void changeColor(){
+        switch (currentColor){
+            case RED:
+                currentColor = Color.GREEN;
+                break;
+            case GREEN:
+                currentColor = Color.YELLOW;
+                break;
+            case YELLOW:
+                currentColor = Color.RED;
+                break;
+        }
+    }
+}
