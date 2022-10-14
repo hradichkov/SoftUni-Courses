85ba7209705ad3b807da2edd2120269bf7761d1f
hradichkov <hristiqnradichkov@gmail.com>
2022-10-14 20:05:50 +0300

    JediGalaxy problem solved

---
+++ OOP/P01WorkingWithAbstractionExercises/P05JediGalaxy/Evil.java
@@ -0,0 +1,13 @@
+package P01WorkingWithAbstractionExercises.P05JediGalaxy;
+
+public class Evil {
+    public static void moveEvil(int currentRowEvil, int currentColEvil, StarsField field) {
+        while (currentRowEvil >= 0 && currentColEvil >= 0) {
+            if (field.isInBounds(currentRowEvil, currentColEvil)) {
+                field.setValue(currentRowEvil, currentColEvil, 0);
+            }
+            currentRowEvil--;
+            currentColEvil--;
+        }
+    }
+}
