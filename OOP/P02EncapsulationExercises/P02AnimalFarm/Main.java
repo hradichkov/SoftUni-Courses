357d5c3bc7bae3a555e9f515e2d3c0417ad51328
hradichkov <hristiqnradichkov@gmail.com>
2022-10-17 17:11:01 +0300

    AnimalFarm problem solved

---
+++ OOP/P02EncapsulationExercises/P02AnimalFarm/Main.java
@@ -0,0 +1,19 @@
+package P02EncapsulationExercises.P02AnimalFarm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        try {
+            Chicken chicken = new Chicken(name, age);
+            System.out.print(chicken);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
