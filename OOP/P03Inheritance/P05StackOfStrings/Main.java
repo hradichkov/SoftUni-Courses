b439449a7773d191f2000b12a454e7b2c42a1156
hradichkov <hristiqnradichkov@gmail.com>
2022-10-19 19:56:44 +0300

    StackOfStrings problem solved

---
+++ OOP/P03Inheritance/P05StackOfStrings/Main.java
@@ -0,0 +1,7 @@
+package P03Inheritance.P05StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ OOP/P03Inheritance/P05StackOfStrings/StackOfStrings.java
@@ -0,0 +1,27 @@
+package P03Inheritance.P05StackOfStrings;
+
+import java.util.ArrayList;
+
+public class StackOfStrings {
+    private ArrayList<String> data;
+
+    public StackOfStrings() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push(String item) {
+        data.add(item);
+    }
+
+    public String pop() {
+        return data.remove(data.size() - 1);
+    }
+
+    public String peek() {
+        return data.get(data.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+}
