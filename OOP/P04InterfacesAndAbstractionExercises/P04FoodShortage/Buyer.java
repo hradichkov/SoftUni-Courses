a314f3dd19155796cea57119a1b82f9a231f2d5a
hradichkov <hristiqnradichkov@gmail.com>
2022-10-23 14:32:33 +0300

    BirthdayCelebrations problem solved

---
+++ OOP/P04InterfacesAndAbstractionExercises/P04FoodShortage/Buyer.java
@@ -0,0 +1,7 @@
+package P04InterfacesAndAbstractionExercises.P04FoodShortage;
+
+public interface Buyer {
+
+    void buyFood();
+    int getFood();
+}
---
+++ OOP/P04InterfacesAndAbstractionExercises/P04FoodShortage/Identifiable.java
@@ -0,0 +1,6 @@
+package P04InterfacesAndAbstractionExercises.P04FoodShortage;
+
+public interface Identifiable {
+
+    String getId();
+}
