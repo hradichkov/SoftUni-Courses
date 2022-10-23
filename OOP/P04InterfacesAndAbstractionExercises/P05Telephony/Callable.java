c043bd90868b4e457377f89fe792ff8ac76cc426
hradichkov <hristiqnradichkov@gmail.com>
2022-10-23 14:34:05 +0300

    Telephony problem solved

---
+++ OOP/P04InterfacesAndAbstractionExercises/P05Telephony/Callable.java
@@ -0,0 +1,6 @@
+package P04InterfacesAndAbstractionExercises.P05Telephony;
+
+public interface Callable {
+
+    String call();
+}
---
+++ OOP/P04InterfacesAndAbstractionExercises/P05Telephony/Smartphone.java
@@ -0,0 +1,61 @@
+package P04InterfacesAndAbstractionExercises.P05Telephony;
+
+import java.util.List;
+
+public class Smartphone implements Callable, Browsable {
+
+    private List<String> numbers;
+    private List<String> urls;
+
+    public Smartphone(List<String> numbers, List<String> urls) {
+        this.numbers = numbers;
+        this.urls = urls;
+    }
+
+    @Override
+    public String browse() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String url : urls) {
+            if (hasNumbers(url)) {
+                sb.append("Invalid URL!").append(System.lineSeparator());
+            } else {
+                sb.append(String.format("Browsing: %s!%n", url));
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String call() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String number : numbers) {
+            if (hasOnlyNumbers(number)) {
+                sb.append(String.format("Calling... %s%n", number));
+            } else {
+                sb.append("Invalid number!").append(System.lineSeparator());
+            }
+        }
+
+        return sb.toString().trim();
+    }
+
+    private boolean hasNumbers(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasOnlyNumbers(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (!Character.isDigit(text.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
