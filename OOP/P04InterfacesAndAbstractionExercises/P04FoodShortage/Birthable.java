705a9c3bcbe68525f4dc5e9a6399dd86d97312f5
hradichkov <hristiqnradichkov@gmail.com>
2022-10-23 14:35:49 +0300

    FoodShortage problem solved

---
+++ OOP/P04InterfacesAndAbstractionExercises/P04FoodShortage/Birthable.java
@@ -0,0 +1,6 @@
+package P04InterfacesAndAbstractionExercises.P04FoodShortage;
+
+public interface Birthable {
+
+    String getBirthDate();
+}
---
+++ OOP/P04InterfacesAndAbstractionExercises/P04FoodShortage/Person.java
@@ -0,0 +1,7 @@
+package P04InterfacesAndAbstractionExercises.P04FoodShortage;
+
+public interface Person {
+
+    String getName();
+    int getAge();
+}
