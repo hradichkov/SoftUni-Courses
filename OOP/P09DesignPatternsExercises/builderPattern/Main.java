95341556f371f93c2e8bfc53f7e796357ba2ba73
hradichkov <hristiqnradichkov@gmail.com>
2022-10-31 17:58:23 +0200

    BuilderPattern

---
+++ OOP/P09DesignPatternsExercises/builderPattern/Main.java
@@ -0,0 +1,48 @@
+package P09DesignPatternsExercises.builderPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final String CREATE_COMMAND = "CREATE";
+    private static final String CONTACT_INFO_COMMAND = "INFO";
+    private static final String DELETE_CONTACT_COMMAND = "DELETE";
+    private static final String PHONEBOOK_COMMAND = "PHONEBOOK";
+    private static final String END_COMMAND = "END";
+
+    public static void main(String[] args) {
+        Contact contact = Contact.builder()
+                .withName("Ivan")
+                .withCompany("IvanOOD")
+                .build();
+    }
+
+    private static List<String> readContactInfo(Scanner input) {
+        List<String> contactInfo = new ArrayList<>();
+
+        System.out.print("Name: ");
+        contactInfo.add(input.nextLine());
+
+        System.out.print("Number: ");
+        contactInfo.add(input.nextLine());
+
+        System.out.print("Company: ");
+        contactInfo.add(input.nextLine());
+
+        System.out.print("Title: ");
+        contactInfo.add(input.nextLine());
+
+        System.out.print("Email: ");
+        contactInfo.add(input.nextLine());
+
+        System.out.print("Website: ");
+        contactInfo.add(input.nextLine());
+
+        System.out.print("Birthday: ");
+        contactInfo.add(input.nextLine());
+
+        return contactInfo;
+    }
+}
