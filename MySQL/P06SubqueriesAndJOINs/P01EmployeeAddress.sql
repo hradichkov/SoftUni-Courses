a5006f39259c701fe2a67fa69df5fe89d1a50329
Hristiqn Radichkov <hristiqnradichkov@gmail.com>
2022-11-22 16:33:25 +0200

    Delete P01EmployeeAddress.sql

--- MySQL/P06SubqueriesAndJOINs/P01EmployeeAddress.sql
+++
@@ -1,6 +0,0 @@
-SELECT e.`employee_id`, e.`job_title`, e.`address_id`, a.`address_text`
-FROM `employees` AS e
-INNER JOIN `addresses` AS a
-ON e.`address_id` = a.`address_id`
-ORDER BY a.`address_id`
-LIMIT 5;
