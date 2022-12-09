a744cf4614fca9e0020d3d07728f5ba88372bdd6
hradichkov <hristiqnradichkov@gmail.com>
2022-12-09 17:31:10 +0200

    Added Constants

---
+++ Spring-Data/P01AccessWithJDBCExercises/Constants.java
@@ -0,0 +1,12 @@
+package P01AccessWithJDBCExercises;
+
+enum Constants {
+    ;
+    static final String USER_KEY = "user";
+    static final String USER_VALUE = "root";
+
+    static final String PASSWORD_KEY = "password";
+    static final String PASSWORD_VALUE = "1234";
+
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/minions_db";
+}
