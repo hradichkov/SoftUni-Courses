0d8c0c4903a0efb6761e999da9d59be17d59520c
hradichkov <hristiqnradichkov@gmail.com>
2022-12-15 17:54:18 +0200

    FindLatest10Projects problem solved

--- Spring-Data/P02IntroductionToHibernate/src/main/java/P09FindLatest10Projects.java
+++ Spring-Data/P02IntroductionToHibernate/src/main/java/P09FindLatest10Projects.java
@@ -1,4 +1,3 @@
-import entities.Employee;
 import entities.Project;
 
 import javax.persistence.EntityManager;
@@ -7,12 +6,9 @@ import javax.persistence.Persistence;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class P09FindLatest10Projects {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("PU_Name");
         EntityManager entityManager = factory.createEntityManager();
 
