fdabc21a30468f3a583af2ca9c02624aaa9e6e13
hradichkov <hristiqnradichkov@gmail.com>
2022-12-15 17:45:43 +0200

    EmployeesWithSalaryOver50000 problem solved

--- Spring-Data/P02IntroductionToHibernate/src/main/java/P04EmployeesWithSalaryOver50000.java
+++ Spring-Data/P02IntroductionToHibernate/src/main/java/P04EmployeesWithSalaryOver50000.java
@@ -1,11 +1,7 @@
-import entities.Employee;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 import java.util.List;
-import java.util.Scanner;
 
 public class P04EmployeesWithSalaryOver50000 {
     public static void main(String[] args) {
