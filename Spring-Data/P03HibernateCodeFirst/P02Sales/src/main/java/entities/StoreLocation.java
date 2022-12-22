9d9a1f5b3b1fc3c02fffe7f341ac8888b49f384d
hradichkov <hristiqnradichkov@gmail.com>
2022-12-22 17:59:03 +0200

    Adding StoreLocation entity

---
+++ Spring-Data/P03HibernateCodeFirst/P02Sales/src/main/java/entities/StoreLocation.java
@@ -0,0 +1,26 @@
+package P03HibernateCodeFirst.P02Sales.src.main.java.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "store_location")
+public class StoreLocation extends BaseEntity{
+
+    @Column
+    private String locationName;
+
+    @OneToMany(mappedBy = "storeLocation")
+    private Set<Sale> sales;
+}
