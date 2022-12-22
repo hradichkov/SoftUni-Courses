5a83bb77b72b28563dec188debe6bd897dab3d9f
hradichkov <hristiqnradichkov@gmail.com>
2022-12-22 17:57:28 +0200

    Adding BaseEntity

---
+++ Spring-Data/P03HibernateCodeFirst/P02Sales/src/main/java/entities/BaseEntity.java
@@ -0,0 +1,19 @@
+package P03HibernateCodeFirst.P02Sales.src.main.java.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+}
