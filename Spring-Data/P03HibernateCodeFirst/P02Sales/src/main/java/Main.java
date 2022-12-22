669e376040643fadc56129d4f4178d7a1a12e927
hradichkov <hristiqnradichkov@gmail.com>
2022-12-22 17:55:40 +0200

    Sales problem solved

---
+++ Spring-Data/P03HibernateCodeFirst/P02Sales/src/main/java/Main.java
@@ -0,0 +1,10 @@
+package P03HibernateCodeFirst.P02Sales.src.main.java;
+
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+         Persistence.createEntityManagerFactory("soft_uni").createEntityManager();
+
+    }
+}
