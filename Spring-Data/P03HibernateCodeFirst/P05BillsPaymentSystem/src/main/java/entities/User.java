7f1ce9083ec799e5d33d56c22d37a1be7a66ea0f
hradichkov <hristiqnradichkov@gmail.com>
2023-01-09 16:49:52 +0200

    Adding User entity

---
+++ Spring-Data/P03HibernateCodeFirst/P05BillsPaymentSystem/src/main/java/entities/User.java
@@ -0,0 +1,30 @@
+package entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class User extends BaseEntity{
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private Set<BillingDetail> billingDetails;
+}
