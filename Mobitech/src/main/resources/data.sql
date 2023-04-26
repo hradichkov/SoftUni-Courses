fdf22966f2326c2ebd402987add747b018d353f5
hradichkov <hristiqnradichkov@gmail.com>
2023-04-26 16:07:18 +0300

    Data.sql added

---
+++ Mobitech/src/main/resources/data.sql
@@ -0,0 +1,42 @@
+USE  mobitech;
+INSERT INTO mobitech.products (name, price, description, img, category_id)
+VALUES ('Смартфон APPLE IPHONE 14',
+        1900,
+        'Super Retina XDR дисплей. Система с две камери: 12MP основна и 12MP Ultra Wide. С TrueDepth камера за лицево разпознаване. Вградена презареждаема литиево-йонна батерия.',
+        'iphone14.jpg',
+        1);
+
+INSERT INTO mobitech.products (name, price, description, img, category_id)
+VALUES ('Смартфон SAMSUNG GALAXY S22',
+        1199,
+        'Най-бързият процесор на Galaxy досега. Nightography камерата улавя детайлите в късните часове на нощта. Споделяй своите шедьоври с едно докосване.',
+        'galaxyS22.jpg',
+        1);
+
+INSERT INTO mobitech.products (name, price, description, img, category_id)
+VALUES ('Телевизор SAMSUNG UE-50BU8572',
+        899,
+        'Реалистични и живи цветове благодарение на минатюрни кристали. По-тънък и елегантен от всякога. Виж всеки нюанс на цвета в мощно 4K качество.',
+        'samsungTv.jpg',
+        3);
+
+INSERT INTO mobitech.products (name, price, description, img, category_id)
+VALUES ('Телевизор SONY KD-50X82K',
+        1449,
+        'Автоматичен режим на картината спрямо жанра. Мощният процесор използва усъвършенстван алгоритъм, за да намали шума и да подобри детайлите. Страхотни 4K картини с реалистични детайли и текстура.',
+        'sonyTv.jpg',
+        3);
+
+INSERT INTO mobitech.products (name, price, description, img, category_id)
+VALUES ('Лаптоп HP 15S-EQ2065NU',
+        1200,
+        'Тънък, лек и създаден за пътувания. Разглеждайте повече от Вашите снимки, видео и проекти на дисплей с 6,5 мм micro-edge рамка.Дългото време на работа на батерията и технологията на НР за бързо зареждане Ви дават възможност да работите, да гледате и да останете свързани през целия ден.',
+        'hp.jpg',
+        2);
+
+INSERT INTO mobitech.products (name, price, description, img, category_id)
+VALUES ('Лаптоп ASUS Vivobook 15',
+        1300,
+        'ASUS Vivobook 15 – лаптопът, оборудван с функциите, които ще улеснят работата ви навсякъде. Всеки аспект на Vivobook 15 е смел и подобрен – от мощния процесор 12 Gen Intel® Core™ до кристално ясния дисплей, пантата с разтвор до 180°, модерните цветове и елегантния геометричен дизайн.',
+        'asus.jpg',
+        2);
